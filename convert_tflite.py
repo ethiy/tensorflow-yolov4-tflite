c00311b452dab2065cafa63f61c7c18b3486879f
oussama <oussama.ennafii@upciti.com>
2022-09-16 15:55:15 +0000

    chore: add black linting

--- convert_tflite.py
+++ convert_tflite.py
@@ -7,52 +7,61 @@ import numpy as np
 import cv2
 import core.utils as utils
 
-flags.DEFINE_string('weights', './checkpoints/yolov4-416', 'path to weights file')
-flags.DEFINE_string('output', './checkpoints/yolov4-416-fp32.tflite', 'path to output')
-flags.DEFINE_integer('input_height', 480, 'image height')
-flags.DEFINE_integer('input_width', 640, 'image width')
-flags.DEFINE_string('quantize_mode', 'float32', 'quantize mode (int8, full-int8, float16, float32)')
-flags.DEFINE_string('dataset', None, 'path to dataset for calibration')
-flags.DEFINE_integer('representative_number', 50, 'number of representative images for calibration.')
+flags.DEFINE_string("weights", "./checkpoints/yolov4-416", "path to weights file")
+flags.DEFINE_string("output", "./checkpoints/yolov4-416-fp32.tflite", "path to output")
+flags.DEFINE_integer("input_height", 480, "image height")
+flags.DEFINE_integer("input_width", 640, "image width")
+flags.DEFINE_string(
+    "quantize_mode", "float32", "quantize mode (int8, full-int8, float16, float32)"
+)
+flags.DEFINE_string("dataset", None, "path to dataset for calibration")
+flags.DEFINE_integer(
+    "representative_number", 50, "number of representative images for calibration."
+)
 
 
 def representative_data_gen():
-  with open(FLAGS.dataset, "r") as image_list_file:
-    image_paths = [Path(line.split("\n")[0]) for line in image_list_file.readlines()]
-    for image_path in random.sample(image_paths, FLAGS.representative_number):
-      if image_path.exists():
-        image = utils.image_preprocess(np.copy(cv2.imread(image_path.as_posix())), [FLAGS.input_height, FLAGS.input_width])
-        yield [image[np.newaxis, ...].astype(np.float32)]
+    with open(FLAGS.dataset, "r") as image_list_file:
+        image_paths = [
+            Path(line.split("\n")[0]) for line in image_list_file.readlines()
+        ]
+        for image_path in random.sample(image_paths, FLAGS.representative_number):
+            if image_path.exists():
+                image = utils.image_preprocess(
+                    np.copy(cv2.imread(image_path.as_posix())),
+                    [FLAGS.input_height, FLAGS.input_width],
+                )
+                yield [image[np.newaxis, ...].astype(np.float32)]
+
 
 def save_tflite():
-  converter = tf.lite.TFLiteConverter.from_saved_model(FLAGS.weights)
-  converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS]
-  if FLAGS.quantize_mode != 'float32':
-    converter.optimizations = [tf.lite.Optimize.DEFAULT]
-    converter.allow_custom_ops = True
-  if FLAGS.quantize_mode == 'float16':
-    converter.target_spec.supported_types = [tf.float16]
-  elif FLAGS.quantize_mode == 'full-int8':
-    converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
-    converter.inference_input_type = tf.uint8
-    converter.inference_output_type = tf.uint8
-  if FLAGS.dataset is not None:
-      converter.representative_dataset = representative_data_gen
-  tflite_model = converter.convert()
-  with open(FLAGS.output, 'wb') as tflite_file:
-    tflite_file.write(tflite_model)
-  logging.info("model saved to: {}".format(FLAGS.output))
+    converter = tf.lite.TFLiteConverter.from_saved_model(FLAGS.weights)
+    converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS]
+    if FLAGS.quantize_mode != "float32":
+        converter.optimizations = [tf.lite.Optimize.DEFAULT]
+        converter.allow_custom_ops = True
+    if FLAGS.quantize_mode == "float16":
+        converter.target_spec.supported_types = [tf.float16]
+    elif FLAGS.quantize_mode == "full-int8":
+        converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
+        converter.inference_input_type = tf.uint8
+        converter.inference_output_type = tf.uint8
+    if FLAGS.dataset is not None:
+        converter.representative_dataset = representative_data_gen
+    tflite_model = converter.convert()
+    with open(FLAGS.output, "wb") as tflite_file:
+        tflite_file.write(tflite_model)
+    logging.info("model saved to: {}".format(FLAGS.output))
 
 
 def main(_argv):
-  with tf.device("cpu"):
-    _ = _argv
-    save_tflite()
+    with tf.device("cpu"):
+        _ = _argv
+        save_tflite()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     try:
         app.run(main)
     except SystemExit:
         pass
-
-
--- detect.py
+++ detect.py
@@ -1,6 +1,7 @@
 import time
 import tensorflow as tf
-physical_devices = tf.config.experimental.list_physical_devices('GPU')
+
+physical_devices = tf.config.experimental.list_physical_devices("GPU")
 if len(physical_devices) > 0:
     tf.config.experimental.set_memory_growth(physical_devices[0], True)
 from absl import app, flags
@@ -14,18 +15,18 @@ import numpy as np
 from tensorflow.compat.v1 import ConfigProto
 from tensorflow.compat.v1 import InteractiveSession
 
-flags.DEFINE_string('framework', 'tf', '(tf, tflite, trt')
-flags.DEFINE_string('weights', './checkpoints/yolov4-416',
-                    'path to weights file')
-flags.DEFINE_integer('height', 480, 'resize images height to')
-flags.DEFINE_integer('width', 640, 'resize images width to')
-flags.DEFINE_boolean('tiny', False, 'yolo or yolo-tiny')
-flags.DEFINE_string('model', 'yolov4', 'yolov3 or yolov4')
-flags.DEFINE_string('image', './data/kite.jpg', 'path to input image')
-flags.DEFINE_string('output', 'result.png', 'path to output image')
-flags.DEFINE_float('iou', 0.45, 'iou threshold')
-flags.DEFINE_float('score', 0.25, 'score threshold')
-flags.DEFINE_string('classes', './data/classes/coco.names', 'File of class names')
+flags.DEFINE_string("framework", "tf", "(tf, tflite, trt")
+flags.DEFINE_string("weights", "./checkpoints/yolov4-416", "path to weights file")
+flags.DEFINE_integer("height", 480, "resize images height to")
+flags.DEFINE_integer("width", 640, "resize images width to")
+flags.DEFINE_boolean("tiny", False, "yolo or yolo-tiny")
+flags.DEFINE_string("model", "yolov4", "yolov3 or yolov4")
+flags.DEFINE_string("image", "./data/kite.jpg", "path to input image")
+flags.DEFINE_string("output", "result.png", "path to output image")
+flags.DEFINE_float("iou", 0.45, "iou threshold")
+flags.DEFINE_float("score", 0.25, "score threshold")
+flags.DEFINE_string("classes", "./data/classes/coco.names", "File of class names")
+
 
 def main(_argv):
     config = ConfigProto()
@@ -40,28 +41,43 @@ def main(_argv):
     original_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB)
 
     image_data = cv2.resize(original_image, (input_height, input_width))
-    image_data = image_data / 255.
+    image_data = image_data / 255.0
 
     images_data = np.asarray([image_data]).astype(np.float32)
 
     start_time = time.time()
-    if FLAGS.framework == 'tflite':
+    if FLAGS.framework == "tflite":
         interpreter = tf.lite.Interpreter(model_path=FLAGS.weights)
         interpreter.allocate_tensors()
         input_details = interpreter.get_input_details()
         output_details = interpreter.get_output_details()
         print(input_details)
         print(output_details)
-        interpreter.set_tensor(input_details[0]['index'], images_data)
+        interpreter.set_tensor(input_details[0]["index"], images_data)
         interpreter.invoke()
-        pred = [interpreter.get_tensor(output_details[i]['index']) for i in range(len(output_details))]
-        if FLAGS.model == 'yolov3' and FLAGS.tiny == True:
-            boxes, pred_conf = filter_boxes(pred[1], pred[0], score_threshold=0.25, input_shape=tf.constant([input_height, input_width]))
+        pred = [
+            interpreter.get_tensor(output_details[i]["index"])
+            for i in range(len(output_details))
+        ]
+        if FLAGS.model == "yolov3" and FLAGS.tiny == True:
+            boxes, pred_conf = filter_boxes(
+                pred[1],
+                pred[0],
+                score_threshold=0.25,
+                input_shape=tf.constant([input_height, input_width]),
+            )
         else:
-            boxes, pred_conf = filter_boxes(pred[0], pred[1], score_threshold=0.25, input_shape=tf.constant([input_height, input_width]))
+            boxes, pred_conf = filter_boxes(
+                pred[0],
+                pred[1],
+                score_threshold=0.25,
+                input_shape=tf.constant([input_height, input_width]),
+            )
     else:
-        saved_model_loaded = tf.saved_model.load(FLAGS.weights, tags=[tag_constants.SERVING])
-        infer = saved_model_loaded.signatures['serving_default']
+        saved_model_loaded = tf.saved_model.load(
+            FLAGS.weights, tags=[tag_constants.SERVING]
+        )
+        infer = saved_model_loaded.signatures["serving_default"]
         batch_data = tf.constant(images_data)
         pred_bbox = infer(batch_data)
         for _, value in pred_bbox.items():
@@ -71,21 +87,30 @@ def main(_argv):
     boxes, scores, classes, valid_detections = tf.image.combined_non_max_suppression(
         boxes=tf.reshape(boxes, (tf.shape(boxes)[0], -1, 1, 4)),
         scores=tf.reshape(
-            pred_conf, (tf.shape(pred_conf)[0], -1, tf.shape(pred_conf)[-1])),
+            pred_conf, (tf.shape(pred_conf)[0], -1, tf.shape(pred_conf)[-1])
+        ),
         max_output_size_per_class=50,
         max_total_size=50,
         iou_threshold=FLAGS.iou,
-        score_threshold=FLAGS.score
+        score_threshold=FLAGS.score,
     )
     print("Runtime:", time.time() - start_time)
-    pred_bbox = [boxes.numpy(), scores.numpy(), classes.numpy(), valid_detections.numpy()]
-    image = utils.draw_bbox(original_image, pred_bbox, classes=utils.read_class_names(FLAGS.classes))
+    pred_bbox = [
+        boxes.numpy(),
+        scores.numpy(),
+        classes.numpy(),
+        valid_detections.numpy(),
+    ]
+    image = utils.draw_bbox(
+        original_image, pred_bbox, classes=utils.read_class_names(FLAGS.classes)
+    )
     image = Image.fromarray(image.astype(np.uint8))
     image.show()
     image = cv2.cvtColor(np.array(image), cv2.COLOR_BGR2RGB)
     cv2.imwrite(FLAGS.output, image)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     try:
         app.run(main)
     except SystemExit:
